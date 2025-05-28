7a29038208b2149068f72396652ad691cf8b7162
fedevitacca <vitaccafede@gmail.com>
2025-05-28 15:46:39 -0300

    colision

---
+++ Assets/New Folder/DeteccionColisiones.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeteccionColisiones : MonoBehaviour
+{
+    public int precio;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.name == "player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
